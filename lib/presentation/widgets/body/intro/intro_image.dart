c09cc3157655cf8c6307d1de51748cb0c858f270
Saad Aslam <saad.aslam@bigwolf.com>
2024-08-28 19:57:10 +0500

    Flutter responsive Web Portfolio

---
+++ lib/presentation/widgets/body/intro/intro_image.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/app_assets.dart';
+import '../../../../core/utils/app_extensions.dart';
+import '../../../../data/models/responsive_size.dart';
+
+class IntroImage extends StatelessWidget {
+  const IntroImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final responsiveSize = ResponsiveSize(
+      deviceWidth: context.width,
+      mobileSize: context.width * .55,
+      ipadSize: context.width * .36,
+      smallScreenSize: context.width * .26,
+    );
+
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        width: responsiveSize.getSize(),
+        height: responsiveSize.getSize(),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.4),
+              blurRadius: 15,
+              spreadRadius: 2.0,
+              offset: const Offset(1, 1),
+            ),
+          ],
+        ),
+        child: Image.asset(
+          AppAssets.devImg,
+          width: responsiveSize.getSize(),
+          height: responsiveSize.getSize(),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
